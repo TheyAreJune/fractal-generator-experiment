ce9e42e3558ff30d5a515802d9a50b5f43392c5c
June P <TheyAreJune@gmail.com>
2021-03-10 12:56:14 -0500

    Messing with some parameters. Saving Result.

--- generation-test.py
+++ generation-test.py
@@ -5,7 +5,7 @@ from PIL import Image
 from numpy import array
   
 # setting the width of the output image as 1024 
-WIDTH = 3840
+WIDTH = 720
   
 # a function to return a tuple of colors 
 # as integer value of rgb 
@@ -16,8 +16,8 @@ def rgb_conv(i):
 # function defining a mandelbrot 
 def mandelbrot(x, y): 
     c0 = complex(x, y) 
-    c = 0.5
-    for i in range(1, 1000): 
+    c = 0.4
+    for i in range(1, 150): 
         if abs(c) > 2: 
             return rgb_conv(i) 
         c = c * c + c0 
@@ -32,10 +32,10 @@ for x in range(img.size[0]):
     # displaying the progress as percentage 
     print("%.2f %%" % (x / WIDTH * 100.0))  
     for y in range(img.size[1]): 
-        pixels[x, y] = mandelbrot((x - (1.0 * WIDTH)) / (WIDTH / 2), 
-                                      (y - (WIDTH / 3.5625)) / (WIDTH / 2)) 
+        pixels[x, y] = mandelbrot((x - (0.75 * WIDTH)) / (WIDTH / 4), 
+                                      (y - (WIDTH / 3.5625)) / (WIDTH / 4)) 
   
 # to display the created fractal after  
 # completing the given number of iterations
-img.save("./mandelbrot.jpg")
+img.save("./mandelbrot_mod.jpg")
 img.show()
